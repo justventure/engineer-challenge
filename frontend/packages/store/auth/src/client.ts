382eecee9023f39becda8a0b45d756af2cf7d633
vwency <veniaminshp@gmail.com>
2026-03-08 00:58:58 +0600

    chore: next build args

--- frontend/packages/store/auth/src/client.ts
+++ frontend/packages/store/auth/src/client.ts
@@ -5,7 +5,7 @@ let clientInstance: GraphQLClient | null = null
 export const getClient = (): GraphQLClient => {
   if (!clientInstance) {
     const url = typeof window === 'undefined'
-      ? process.env.INTERNAL_GRAPHQL_URL!
+      ? process.env.NEXT_INTERNAL_GRAPHQL_URL!
       : process.env.NEXT_PUBLIC_GRAPHQL_URL!
 
     clientInstance = new GraphQLClient(url, {
